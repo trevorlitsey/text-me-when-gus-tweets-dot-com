b68d2b32a07c3f293be3277528d67cebf7431997
Trevor Litsey <trevorlitsey@gmail.com>
2019-08-03 23:22:32 -0500

    whoops port

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ app.post('/check-tweets', async (_req, res) => {
   }
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 http.createServer(app).listen(PORT, () => {
   console.log('> app listening on http://localhost:' + PORT);
 });
